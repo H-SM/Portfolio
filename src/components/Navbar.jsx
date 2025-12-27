eda7dd100adf452f1f1394f44392d2e69e5b269d
Harman Singh <harmanmalht777@gmail.com>
2025-12-27 05:50:11 +0530

    chore: fix over navbar content on uri

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             >
               <div className="w-full flex justify-between items-center cursor-pointer z-20 ">
                 <p className="font-bold text-white text-[0.8rem]">
-                  HARMANCODES.COM
+                  THISISHARMAN.COM
                 </p>
                 {/* <img
                   src={!opener ? menu : close}
